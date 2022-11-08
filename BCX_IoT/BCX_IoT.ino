04cf24c707ecf111f18334fbfe58ecda3a5b1ed5
Jhonys <jhonys.oliveira@hotmail.com>
2022-11-08 17:12:14 +0000

    Add files via upload

--- BCX_IoT/BCX_IoT.ino
+++ BCX_IoT/BCX_IoT.ino
@@ -1,12 +1,11 @@
-// Adafruit Circuit Playground - Version: Latest
-// #include <Adafruit_CircuitPlayground.h>
-
 #include <ArduinoJson.h>
 
 #define NUM_DEVICE 1
-#define NUM_LEDS 10
 
-int RGB1_PIN[3] = { 3, 5, 6 };
+int num_leds = 3;
+int RGB_PINOUT[num_leds][3] = { { 3, 5, 6 },
+                                { 9, 10, 11 },
+                                { A0, A1, A2 } };
 
 const int rows = 3;
 const int columns = 3;
@@ -16,34 +15,77 @@ int LIGHT_TEMP[rows][columns] = { { 255, 255, 255 },
 
 StaticJsonDocument<200> doc;
 
-int light_intensity;            // 0 to 255
 int light_r, light_g, light_b;  // 0 to 255
 float temperature = 0.0f;
 
 void setup() {
+  init_packet_message();
   Serial.begin(9600);
 }
 
 void loop() {
 
-  for (int i = 0; i < rows; i++) {
-    setLightIntensity(LIGHT_TEMP[i][0], LIGHT_TEMP[i][1], LIGHT_TEMP[i][2]);
-    delay(500);
+  if (Serial.available() > 0) {
+    String s = Serial.readStringUntil('\n');
+    DeserializationError error = deserializeJson(doc, s);
+
+    // Test if parsing succeeds.
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return;
+    }
+
+    setLightIntensity(doc["light_r"], doc["light_g"], doc["light_b"]);
+  }
+
+  serializeJson(doc, Serial);
+  Serial.println();
+}
+
+
+void init_packet_message() {
+  // {"num_device":1,"temperature":0,"light_r":255,"light_g":255,"light_b":255}
+  // {"num_device":1,"temperature":0,"light_r":255,"light_g":142,"light_b":33}
+  // {"num_device":1,"temperature":0,"light_r":255,"light_g":56,"light_b":0}
+
+  // num_device
+  doc["num_device"] = NUM_DEVICE;
+  doc["temperature"] = 0;
+
+  doc["light1_r"] = 0;
+  doc["light1_g"] = 255;
+  doc["light1_b"] = 0;
+
+  doc["light2_r"] = 0;
+  doc["light2_g"] = 255;
+  doc["light2_b"] = 0;
+
+  doc["light3_r"] = 0;
+  doc["light3_g"] = 255;
+  doc["light3_b"] = 0;
+
+  for (int i = 0; i < num_leds; i++) {
+    setLightIntensity(i, doc["light_r"], doc["light_g"], doc["light_b"]);
   }
 }
 
+void setLightIntensity(int num_led, int r, int g, int b) {
+  analogWrite(RGB_PINOUT[num_led][0], r);
+  analogWrite(RGB_PINOUT[num_led][1], g);
+  analogWrite(RGB_PINOUT[num_led][2], b);
 
-String message() {
-  // num_device
-  doc["num_device"] = NUM_DEVICE;
-  doc["light_r"] = light_r;
-  doc["light_g"] = light_g;
-  doc["light_b"] = light_b;
-  doc["temperature"] = temperature;
-}
-
-void setLightIntensity(int r, int g, int b) {
-  analogWrite(RGB1_PIN[0], r);
-  analogWrite(RGB1_PIN[1], g);
-  analogWrite(RGB1_PIN[2], b);
+  if (num_led == 0) {
+    doc["light1_r"] = r;
+    doc["light1_g"] = g;
+    doc["light1_b"] = b;
+  } else if (num_led == 0) {
+    doc["light2_r"] = r;
+    doc["light2_g"] = g;
+    doc["light2_b"] = b;
+  } else if (num_led == 0) {
+    doc["light3_r"] = r;
+    doc["light3_g"] = g;
+    doc["light3_b"] = b;
+  }
 }
