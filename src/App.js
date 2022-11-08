eb3973876b7779769820ac939c683b425fb29a9f
Adil Raza <adilraza2@hotmail.com>
2022-11-08 13:56:18 +0100

    Routes + Lights Table added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Layout from "./components/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Settings from "./components/Settings";
+import SensorList from "./components/SensorList";
+import Performance from "./components/Performance";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div>
-      <Layout />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="lights" element={<SensorList />} />
+          <Route path="performance" element={<Performance />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
