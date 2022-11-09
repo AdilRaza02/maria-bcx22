65f692971a91bff3aac47c7e16a6f20b97a57260
Adil Raza <adilraza2@hotmail.com>
2022-11-09 14:44:40 +0100

    DashboardTable Added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Map from "../assets/map1.jpg";
+import DashboardTable from "./DashboardTable";
+import { getLatestTempDashboard } from "../services";
 
 export default function Dashboard() {
-
-
   const sensorBaseStyle = {
     width: "80px",
     height: "50px",
@@ -12,73 +12,96 @@ export default function Dashboard() {
   const sensorOneStyle = {
     top: "130px",
     left: "150px",
-    ...sensorBaseStyle
+    ...sensorBaseStyle,
   };
 
   const sensorTwoStyle = {
     top: "135px",
     left: "385px",
-    ...sensorBaseStyle
+    ...sensorBaseStyle,
   };
-  
+
   const sensorThreeStyle = {
     top: "245px",
     left: "140px",
-    ...sensorBaseStyle
+    ...sensorBaseStyle,
   };
-  
+
   const sensorFourStyle = {
     top: "245px",
     left: "390px",
-    ...sensorBaseStyle
+    ...sensorBaseStyle,
   };
-  
+
   const sensorFiveStyle = {
     bottom: "135px",
     left: "130px",
-    ...sensorBaseStyle
+    ...sensorBaseStyle,
   };
-  
+
   const sensorSixStyle = {
     bottom: "135px",
     left: "265px",
-    ...sensorBaseStyle
+    ...sensorBaseStyle,
   };
-  
+
   const sensorSevenStyle = {
     bottom: "135px",
     left: "395px",
-    ...sensorBaseStyle
+    ...sensorBaseStyle,
   };
 
+  const [deviceData, setDeviceData] = React.useState([]);
+
+  const getLatestConnectedDevicesData = async () => {
+    try {
+      const { data } = await getLatestTempDashboard();
+      const sortedData = data.sort((e1, e2) => e1.deviceId.toLowerCase().localeCompare(e2.deviceId.toLowerCase()));
+      setDeviceData(sortedData);
+      if (!sortedData.length) {
+        alert("No Devices Connected!");
+      }
+    } catch (error) {
+      console.log(error)
+      alert(`Something went wrong! Code: ${error.code}`);
+    }
+  };
+
+  React.useEffect(() => {
+    getLatestConnectedDevicesData();
+    // setInterval(() => {
+    //   getLatestConnectedDevicesData();
+    // }, 5000);
+  }, []);
 
   return (
-    <div className="relative">
-      <div onClick={()=>alert("Sensor 1")} style={sensorOneStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
-        Sensor 1
+    <div className="relative flex items-center">
+      <img height={200} width={200} src={Map} alt="map" className="w-fit" />
+      <div onClick={() => alert("Device 1")} style={{ ...sensorOneStyle }} className="text-white text-sm p-2 text-center absolute cursor-pointer">
+        Device01
       </div>
-      <div onClick={()=>alert("Sensor 2")} style={sensorTwoStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
-        Sensor 2
-      </div>
-
-      <div onClick={()=>alert("Sensor 3")} style={sensorThreeStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
-        Sensor 3
+      <div style={sensorTwoStyle} className="text-white text-xs p-2 text-center absolute cursor-pointer">
+        Device N/A
       </div>
 
-      <div onClick={()=>alert("Sensor 4")} style={sensorFourStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
-        Sensor 4
-      </div>
-      <div onClick={()=>alert("Sensor 5")} style={sensorFiveStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
-        Sensor 5
-      </div>
-      <div onClick={()=>alert("Sensor 6")} style={sensorSixStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
-        Sensor 6
-      </div>
-      <div onClick={()=>alert("Sensor 7")} style={sensorSevenStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
-        Sensor 7
+      <div style={sensorThreeStyle} className="text-white text-xs p-2 text-center absolute cursor-pointer">
+        Device N/A
       </div>
 
-      <img height={1000} width={600} src={Map} alt="map" />
+      <div style={sensorFourStyle} className="text-white text-xs p-2 text-center absolute cursor-pointer">
+        Device N/A
+      </div>
+      <div onClick={() => alert("Device 5")} style={sensorFiveStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
+        Device03
+      </div>
+      <div style={sensorSixStyle} className="text-white text-xs p-2 text-center absolute cursor-pointer">
+        Device N/A
+      </div>
+      <div onClick={() => alert("Device 7")} style={sensorSevenStyle} className="text-white text-sm p-2 text-center absolute cursor-pointer">
+        Device02
+      </div>
+
+      {deviceData.length ? <DashboardTable devices={deviceData} className="w-fit" /> : <span></span>}
     </div>
   );
 }
---
+++ src/components/DashboardTable.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
+
+function DashboardTable({ devices }) {
+  return (
+    <>
+      <div className="px-4">
+        <div className="border rounded-lg border pb-6 border-gray-200">
+          <div className="flex items-center border-b border-gray-200 justify-between px-6 py-3">
+            <p className="text-sm lg:text-xl font-semibold leading-tight text-gray-800">Connected Devices</p>
+          </div>
+          <div className="px-6 pt-6 overflow-x-auto">
+            <table className=" whitespace-nowrap">
+              <tbody>
+                {devices.map((device, index) => {
+                  return (
+                    <tr key={uuidv4()} className="p-6">
+                      <td className="p-3">
+                        <div className="flex items-center">
+                          <div className="bg-gray-800 rounded-sm p-1">
+                            <p className="text-white"> {index + 1}</p>
+                          </div>
+                          <div className="pl-3">
+                            <div className="flex items-center text-sm leading-none">
+                              <p className="text-blue-500 mr-1">(ID)</p>
+                              <p className="font-semibold text-gray-800">{device.deviceId}</p>
+                            </div>
+                            <p className="text-md leading-none text-gray-600 mt-2">{device.temp.toFixed(1)}°C</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="pl-16">
+                        <div>
+                          <p className="text-sm font-semibold leading-none text-right text-gray-800">{moment(device.createdAt).fromNow()}</p>
+                          <div className="flex items-center space-x-1 mt-1">
+                            <div className="h-4 w-4 rounded-full border" style={{ backgroundColor: `#${device.color.toString(16)}` }}></div>
+                            <p className="text-sm text-gray-700">Room Light Color</p>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default DashboardTable;
