7cd3489559cb3a150b8e5b35f950cfcb4ae71755
Jhonys <jhonys.oliveira@hotmail.com>
2022-11-09 09:49:17 +0100

    Update BCX_IoT.ino

--- BCX_IoT_Adafruit/BCX_IoT.ino
+++ BCX_IoT_Adafruit/BCX_IoT.ino
@@ -3,26 +3,31 @@
 
 #define NUM_DEVICE 1
 
-int num_leds = 3;
-
-const int rows = 3;
-const int columns = 3;
-
 StaticJsonDocument<200> doc;
+StaticJsonDocument<200> doc_from_serial;
 
-int light_r, light_g, light_b;  // 0 to 255
-float temperature = 0.0f;
+// message always sending
+// {"num_device":1,"temperature":24.84582901,"light_r":255,"light_g":142,"light_b":33,"light_intensity":128}
 
 void setup() {
+
+  CircuitPlayground.begin();
+
   init_packet_message();
   Serial.begin(9600);
 }
 
-void setLightIntensity(int r, int g, int b) {
+void setLightColor(int value) {
+  CircuitPlayground.strip.setBrightness(value);  // 0 to 255
+  CircuitPlayground.strip.show();
+}
+
+void setLightColor(int r, int g, int b) {
 
   for (int i = 0; i < 10; i++) {
-    CircuitPlayground.setPixelColor(i, r, g, b);
+    CircuitPlayground.strip.setPixelColor(i, r, g, b);
   }
+  CircuitPlayground.strip.show();
   doc["light_r"] = r;
   doc["light_g"] = g;
   doc["light_b"] = b;
@@ -30,11 +35,14 @@ void setLightIntensity(int r, int g, int b) {
 
 void loop() {
 
-  CircuitPlayground.begin();
+  while (!Serial)
+    return;
 
   if (Serial.available() > 0) {
+
+    // receiving
     String s = Serial.readStringUntil('\n');
-    DeserializationError error = deserializeJson(doc, s);
+    DeserializationError error = deserializeJson(doc_from_serial, s);
 
     // Test if parsing succeeds.
     if (error) {
@@ -43,18 +51,30 @@ void loop() {
       return;
     }
 
-    setLightIntensity(doc["light_r"], doc["light_g"], doc["light_b"]);
+    doc["light_r"] = doc_from_serial["light_r"];
+    doc["light_g"] = doc_from_serial["light_g"];
+    doc["light_b"] = doc_from_serial["light_b"];
+
+    setLightColor(doc["light_r"], doc["light_g"], doc["light_b"]);
+
+    setLightColor(doc["light_intensity"]);
   }
 
+  // sending
+  doc["temperature"] = CircuitPlayground.temperature();
   serializeJson(doc, Serial);
   Serial.println();
 }
 
 
 void init_packet_message() {
-  // {"num_device":1,"temperature":0,"light_r":255,"light_g":255,"light_b":255}
-  // {"num_device":1,"temperature":0,"light_r":255,"light_g":142,"light_b":33}
-  // {"num_device":1,"temperature":0,"light_r":255,"light_g":56,"light_b":0}
+  // example to send
+  // white cold
+  // {"light_r":255,"light_g":255,"light_b":255,"light_intensity":255}
+  // white mid
+  // {"light_r":255,"light_g":142,"light_b":33,"light_intensity":255}
+  // white warm
+  // {"light_r":255,"light_g":56,"light_b":0,"light_intensity":255}
 
   // num_device
   doc["num_device"] = NUM_DEVICE;
@@ -64,5 +84,9 @@ void init_packet_message() {
   doc["light_g"] = 255;
   doc["light_b"] = 0;
 
-  setLightIntensity(doc["light_r"], doc["light_g"], doc["light_b"]);
+  doc["light_intensity"] = 128;
+
+  setLightColor(doc["light_r"], doc["light_g"], doc["light_b"]);
+
+  CircuitPlayground.strip.setBrightness(doc["light_intensity"]);  // 0 to 255
 }
